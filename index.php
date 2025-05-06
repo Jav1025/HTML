7147bef84e442445e3cf87defc4c43f6bb00a53d
javier <am1545012010@unab.edu.sv>
2025-05-06 16:47:20 -0600

    subiendo archivos

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tabla.css">
+    <title>Estilo de las tablas</title>
+</head>
+<body>
+
+<h1>Bienvenido a la web tabla</h1>
+
+
+<button type="button" class="btn btn-dark">Editar</button>
+    <div id="main-container">
+    <table>
+<thead>
+    <tr>
+    <th>Descripcion</th><th>Mes</th><th>costo</th><th>Si</th><th>No</th><th>Porque</th>
+    </tr> 
+</thead>
+    <tr>
+    <th>Agua</th><th>Enero</th><th>$5000</th><th>a</th><th>a</th><th> a</th>
+    </tr> 
+    <tr>
+    <th>Luz</th><th>Febrero</th><th>$5,500</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <tr>
+    <th>Carro</th><th>Marzo</th><th>$10,000</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <tr>
+    <th>Universidad</th><th>Abril</th><th>$4500</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <tr>
+    <th>trasporte</th><th>Mayo</th><th>$100</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <th>Fiesta</th><th>Junio</th><th>$4500</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <tr>
+    <th>Viajes</th><th>Julio</th><th>$100</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <tr>
+    <th>Familia</th><th>Agosto</th><th>$100</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <tr>
+    <th>Vestimenta casual</th><th>Septiembre</th><th>$160</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <th>Vestimenta formal</th><th>Octubre</th><th>$4500</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <tr>
+    <th>Cumpleaños</th><th>Noviembre</th><th>$100</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    <tr>
+    <th>Internet</th><th>Diciembre</th><th>$100</th><th>a</th><th>a</th><th>a</th>
+    </tr> 
+    </table>
+
+
+</body>
+</html>   
